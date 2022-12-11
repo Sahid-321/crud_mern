26f46786563ab127d68304179fae0e5a9ab83403
sahid jamal <sahidjamal151@gmail.com>
2022-12-11 23:57:08 +0530

    axios added

--- crud/src/CreatePost.js
+++ crud/src/CreatePost.js
@@ -1,6 +1,7 @@
 import { Button, Form } from "react-bootstrap";
 import {useNavigate} from "react-router-dom"
 import {useState} from "react"
+import axios from "axios"
 
 const CreatePost = ()=>{
     const [post, setPost] = useState({
@@ -18,16 +19,27 @@ const CreatePost = ()=>{
 
        })
     }
+
+    const handleClick = (event)=>{
+        event.preventDefault();
+axios.post("/create", post)
+.then((res)=> console.log(res))
+.catch((err)=> console.log(err))
+
+    }
     const navigate = useNavigate()
     return (
         <div style={{width: "98%", margin: "auto auto", textAlign: "center"} }>
 
         <h1>cerate Post</h1>
         <Form>
+            <Form.Group>
             <Form.Control
              value={post.title} onChange={handleChange} style={{marginBottom:"1rem"}} name="title" placeholder="Title" />
             <Form.Control
              value={post.description} onChange={handleChange} style={{marginBottom:"1rem"}} name="description" placeholder="Description" />
+       </Form.Group>
+       <Button style={{width: "100%", marginButton: "1rem"}} variant="outline-success" onClick={handleClick}>Create Post</Button>
         </Form>
         <Button style={{width:"100%"}}  variant="outline-dark" onClick={()=> navigate(-1)} >Back</Button>
         </div>
