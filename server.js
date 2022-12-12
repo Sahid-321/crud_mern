e3e55c0664f6b6b0b759c72f97d9a6762dadc22e
sahid jamal <sahidjamal151@gmail.com>
2022-12-12 10:17:35 +0530

    mongoDB data added

--- server.js
+++ server.js
@@ -4,8 +4,34 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 8080;
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use(cors())
+
+mongoose.connect(`mongodb://localhost:27017/crud`)
+.catch((err)=> console.log(err))
+
+//Schema
+
+const postSchema = mongoose.Schema({
+    title: String,
+    description: String,
+})
+
+const Post = mongoose.model('post', postSchema);
+
+
 app.get('/', (req,res)=>{
     res.send('server working sahid')
+})
+
+app.post('/create', (req,res)=>{
+    Post.create({
+title: req.body.title,
+description: req.body.description
+    })
+    .then((doc)=> console.log(doc))
+    .catch((err)=> console.log(err))
 })
 
 app.listen(PORT, ()=>{
