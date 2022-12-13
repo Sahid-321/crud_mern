8d16355951233f13bd2cb00856d848417560c87f
sahid jamal <sahidjamal151@gmail.com>
2022-12-13 23:08:36 +0530

    post page created:

--- practice_crud/democrud/src/App.js
+++ practice_crud/democrud/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home';
+import { Route, Routes } from 'react-router-dom';
+import CreatePost from './components/createPost';
 
 function App() {
   return (
     <div className="App">
-   <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/create' element={<CreatePost/>} />
+      </Routes>
+
     </div>
   );
 }
---
+++ practice_crud/democrud/src/components/createPost.jsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap"
+
+const CreatePost = ()=>{
+    return(
+
+        <>
+        <h1>Create Post</h1>
+        <Button  >ADD</Button>
+        </>
+    )
+}
+export default CreatePost;
