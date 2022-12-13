4a5a58644b9a789f652bf5d7b150582dd01b6035
sahid jamal <sahidjamal151@gmail.com>
2022-12-13 23:30:18 +0530

    from created

--- practice_crud/democrud/src/components/createPost.jsx
+++ practice_crud/democrud/src/components/createPost.jsx
@@ -1,11 +1,37 @@
-import { Button } from "react-bootstrap"
-
+import { Button, Form } from "react-bootstrap"
+import { useState } from "react";
 const CreatePost = ()=>{
+const[post, setPost] = useState({
+    title: "",
+    description:""
+})
+const handleChange = (event)=>{
+   const {name, value} = event.target;
+   console.log(event.target.value);
+    setPost((prev)=> {
+       
+        return{
+            ...prev,
+            [name]:value
+        }
+    })
+}
+
+const handleClick = ()=>{
+    console.log(post);
+}
     return(
 
         <>
         <h1>Create Post</h1>
-        <Button  >ADD</Button>
+<Form>
+    <Form.Group>
+        <Form.Control value={post.title}  onChange={handleChange} name="title" placeholder="Title" />
+        <Form.Control value={post.description} onChange={handleChange} name="description" placeholder="Description" />
+    </Form.Group>
+    <Button onClick={handleClick} >ADD</Button>
+</Form>
+        
         </>
     )
 }
