bda0c6c8a81b0d0474e0f1c2fa7e28982728a4fb
sahid jamal <sahidjamal151@gmail.com>
2022-12-14 15:36:08 +0530

    modal bootsrap added

--- practice_crud/democrud/src/components/post.jsx
+++ practice_crud/democrud/src/components/post.jsx
@@ -1,37 +1,76 @@
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios"
+import Modal from 'react-bootstrap/Modal';
 
-const Post  = ()=>{
-const [post, setPost] = useState([]);
+const Post = () => {
+    const [show, setShow] = useState(false);
 
-useEffect(()=>{
-axios.get("http://localhost:8000/posts")
-.then((res)=> {console.log(res.data)
-    setPost(res.data)
-})
-.catch((err)=> console.log(err))
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-},[])
+    const [post, setPost] = useState([]);
 
+    useEffect(() => {
+        axios.get("http://localhost:8000/posts")
+            .then((res) => {
+                console.log(res.data)
+                setPost(res.data)
+            })
+            .catch((err) => console.log(err))
+
+    }, [])
+
+    const handleUpdate = () => {
+handleShow();
+    }
+
+    const handleDelete = () => {
+
+    }
     const navigate = useNavigate();
     return (
         <>
-        <h1>Data</h1>
+            <h1>Data</h1>
 
-        <Button onClick={()=>navigate(-1)} >BACK</Button>
+            <Button onClick={() => navigate(-1)} >BACK</Button>
+            <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update user</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+                <Form.Group>
+                    <Form.Control placeholder="Title" />
+                    <Form.Control placeholder="Description" />
+                </Form.Group>
+            </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+            {
+                post.map((elem) => {
+                    return (
+                        <div key={elem._id} style={{ width: "90%", border: "solid blue 1px" }}>
+                            <h4>{elem.title}</h4>
+                            <p>{elem.description}</p>
 
-        {
-            post.map((elem)=>{
-                return(
-                    <div key={elem._id} style={{width:"90%", border:"solid blue 1px"}}>
-                    <h4>{elem.title}</h4>
-                    <p>{elem.description}</p>
-                    </div>
-                )
-            })
-        }
+                            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                <Button onClick={handleUpdate} variant="info">Update</Button>
+                                <Button onClick={handleDelete} variant="danger">Delete</Button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </>
     )
 }
