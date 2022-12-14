ddce6e0d3452706a44e9e64b347e00e327a8b431
sahid jamal <sahidjamal151@gmail.com>
2022-12-14 12:02:04 +0530

    post method created

--- practice_crud/democrud/src/components/post.jsx
+++ practice_crud/democrud/src/components/post.jsx
@@ -1,7 +1,12 @@
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
 const Post  = ()=>{
+    const navigate = useNavigate();
     return (
         <>
         <h1>Data</h1>
+        <Button onClick={()=>navigate(-1)} >BACK</Button>
         </>
     )
 }
--- practice_crud/index.js
+++ practice_crud/index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors')
 const PORT = 8080;
+
+mongoose.connect(`mongodb://localhost:27017/democrud`)
+.catch((err)=> console.log(err))
+
+const postSchema = mongoose.Schema({
+    title:String,
+    description: String
+})
+
+const Post = mongoose.model("crudpost", postSchema);
+
+app.post("/create", (req,res)=>{
+    Post.create({
+        title: req.body.title,
+        description:req.body.description
+    })
+    .then((res)=> console.log(res))
+    .catch((err)=> console.log(err))
+})
 
 app.listen(PORT,()=>{
     console.log(`server running at port${PORT}`);
