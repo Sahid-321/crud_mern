4d56e44c05d064189b4c47374da7282546835724
sahid jamal <sahidjamal151@gmail.com>
2022-12-22 11:24:43 +0530

    router working fine

--- practice_crud1/crud/src/App.js
+++ practice_crud1/crud/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './components/Home';
+import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+<Routes>
+<Route path='/' element={<Home/>} />
+
+</Routes>
 
     </div>
   );
