0a0911e62b970b6dfb6348773d0d3a309508f9aa
sahid jamal <sahidjamal151@gmail.com>
2022-12-22 11:10:38 +0530

    Home page created

---
+++ practice_crud1/crud/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import{Button} from 'react-bootstrap'
+const Home = ()=>{
+
+    return(
+        <>
+        <h1>
+            Home Page 
+        </h1>
+<Button>Go to the next</Button>
+        </>
+    )
+}
+
+export default Home
