de2234f7ca743066a13c8d2e582d2210c68561ff
sahid jamal <sahidjamal151@gmail.com>
2022-12-22 14:59:18 +0530

    backend created

--- practice_crud1/crud/src/components/CreatePost.jsx
+++ practice_crud1/crud/src/components/CreatePost.jsx
@@ -1,14 +1,49 @@
-import { Button } from "react-bootstrap";
+import { useState } from "react";
+import { Button,Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios'
 const CreatePost = ()=>{
     const navigate = useNavigate()
+const [postData, setPostData] = useState({
+    title: "",
+    description: ""
+})
+const handleChange = (event)=>{
+const {name, value} = event.target;
+setPostData((prev)=>{
+return {
+    ...prev,
+[name]:value
+}
+}
+
+
+)
+console.log(postData);
+}
+
+
+const handleClick = (event)=>{
+    event.preventDefault();
+  axios.post('/post',postData)
+  .then((res) =>console.log(res))
+  .catch((err) => {
+    console.log(err)
+  });
+}
     return(
         <>
         <h1>
             Create Your Post
         </h1>
+        <Form>
+            <Form.Group>
+                <Form.Control value={postData.title} onChange={handleChange} name="title" placeholder="Title" />
+                <Form.Control value={postData.description} onChange={handleChange} name="description" placeholder="Description" />
+            </Form.Group>
+        </Form>
 
-        <Button onClick={()=> navigate('/post/data')}>NEXT</Button>
+        <Button onClick={handleClick}>NEXT</Button>
         </>
     )
 }
